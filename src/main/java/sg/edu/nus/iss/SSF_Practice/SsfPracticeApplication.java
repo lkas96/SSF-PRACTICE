da56df82c3d440338be0bcde23b837f0806bf5dd
lkas96 <kwek_lawson@hotmail.com>
2024-12-04 19:49:14 +0800

    Springboot Template

---
+++ src/main/java/sg/edu/nus/iss/SSF_Practice/SsfPracticeApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.SSF_Practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsfPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsfPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/SSF_Practice/SsfPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.SSF_Practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsfPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
